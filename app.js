78933813b061fbd4ca5b25d6248ad6e51d69d5e1
Ben de Garcia <degarcia.ben@gmail.com>
2020-07-18 15:19:06 -0500

    update

--- app.js
+++ app.js
@@ -4,32 +4,69 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+const questions = require("./lib/questions")
+const render = require("./lib/htmlRenderer")
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+let id = 0
+
+function askQuestions(){
+    return inquirer.prompt(questions.general)
+};
+function askManagerQuestions(){
+    return inquirer.prompt(questions.Manager)
+};
+function askEngineerQuestions(){
+    return inquirer.prompt(questions.Engineer)
+};
+function askInternQuestions(){
+    return inquirer.prompt(questions.Intern)
+}
+function otraVez(){
+    return inquirer.prompt(questions.again)
+}
+
+async function employeeList(){
+    const managerEmp = {};
+    const internList = [];
+    const engineerList = [];
+    const again = true;
+    const em = [managerEmp, internList, engineerList]
+
+    while(again){
+    id += 1;
+    let employee = await askQuestions();
+    let employeeFullName = `${employee.firstname} ${employee.lastname}`
+    let employeeEmail = employeeFullName.trim() + '@working.com'
+        console.log(employee.role)
+    switch(employee.role){
+        case 'Manager':
+            let manRoleQuestions = await askManagerQuestions();
+            employee = new Manager(employeeFullName, id, employeeEmail, manRoleQuestions.office)
+            managerEmp.push(employee);
+            break;
+        case 'Intern':
+            let intRoleQuestions = await askInternQuestions();
+            employee = new Intern(employeeFullName, id, employeeEmail, intRoleQuestions.school)
+            internList.push(employee);
+            break;
+        case 'Engineer':
+            let engRoleQuestions = await askEngineerQuestions();
+            employee = new Engineer(employeeFullName, id, employeeEmail, engRoleQuestions.github)
+            engineerList.push(employee);
+            break;
+        };
+
+        again = otraVez();
+        console.log(em)
+    }
+    const html = render(em);
+    fs.writeFile(outputPath, html, (err) => {
+        if(err) throw err;
+        console.log('Great Success');        
+    })
+}
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+employeeList();
